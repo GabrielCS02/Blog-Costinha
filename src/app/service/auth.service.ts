4eef6e38031e2c593e43969d2343779c3e7a9844
Gabriel Costa <gacosil0204@gmail.com>
2022-01-28 17:50:56 -0300

    Implementação do post e get de postagem

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
@@ -14,12 +14,26 @@ export class AuthService {
     private http: HttpClient
   ) { }
 
+  token={
+    headers: new HttpHeaders().set('Authorization', environment.token),
+  };
+
+  refreshToken(){
+    this.token = {
+      headers: new HttpHeaders().set('Authorization', environment.token),
+    };
+  }
+
   login(userLogin: UserLogin): Observable<UserLogin>{
     return this.http.post<UserLogin>("https://blogcosta.herokuapp.com/usuarios/logar", userLogin)
   }
 
   cadastro(user: User): Observable<User>{
     return this.http.post<User>("https://blogcosta.herokuapp.com/usuarios/cadastrar", user)
+  }
+
+  getByIdUser(id: number): Observable<User>{
+    return this.http.get<User>(`https://blogcosta.herokuapp.com/usuarios/${id}`, this.token)
   }
 
   logado(){
